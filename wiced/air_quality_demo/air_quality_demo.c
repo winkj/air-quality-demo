52b1a983ce959f5f95b67b5b3a297d251f12a6fe
Johannes Winkelmann <jwi@sensirion.com>
2019-02-06 15:43:09 -0800

    add comments, remove LED handling

--- wiced/air_quality_demo/air_quality_demo.c
+++ wiced/air_quality_demo/air_quality_demo.c
@@ -28,6 +28,15 @@
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/*
+ * This is based on the example usage for the SPS30 from it's library
+ *  - https://github.com/Sensirion/embedded-sps
+ *
+ * as well as the Sensirion ESS library
+ *  - https://github.com/winkj/wiced-ess
+ */
+
+
 #include "wiced.h"
 #include "http_client.h"
 
@@ -76,6 +85,8 @@ void application_start( )
             .leds_supported        = 0,
     };*/
 
+
+    // -- device probing
     while (ess_init(&ESS_DEVICE_CONFIG_NEBULA) != WICED_SUCCESS) {
         WPRINT_APP_INFO(("ESS probing failed\n"));
         wiced_rtos_delay_milliseconds(DELAY_TIME);
@@ -87,6 +98,7 @@ void application_start( )
     }
     WPRINT_APP_INFO(("SPS sensor probing successful\n"));
 
+    // -- setup SPS30 sensor
     ret = sps30_set_fan_auto_cleaning_interval_days(auto_clean_days);
     if (ret)
         WPRINT_APP_INFO(("error %d setting the auto-clean interval\n", ret));
@@ -97,7 +109,9 @@ void application_start( )
     WPRINT_APP_INFO(("measurements started\n"));
     wiced_rtos_delay_milliseconds(1000);
 
-    while (1) {
+    // -- main loop
+        // -- read SGP30, connected to the ESS (environmental sensor shield)
+        while (1) {
         wiced_result_t ret = ess_measure_iaq(&tvoc_ppb, &co2_eq_ppm);
         if (ret == WICED_SUCCESS) {
             WPRINT_APP_INFO(("tVOC %4u | CO2eq %4u | ", tvoc_ppb, co2_eq_ppm));
@@ -105,14 +119,7 @@ void application_start( )
             WPRINT_APP_INFO(("error reading IAQ values\n"));
         }
 
-        if (co2_eq_ppm > 3000) {
-            ess_set_leds_ryg(1, 0, 0);
-        } else if (co2_eq_ppm > 1000) {
-            ess_set_leds_ryg(0, 1, 0);
-        } else {
-            ess_set_leds_ryg(0, 0, 1);
-        }
-
+        // -- read SHTC1, connected to the ESS (environmental sensor shield)
         ret = ess_measure_rht(&temperature, &humidity);
         if (ret == WICED_SUCCESS) {
             WPRINT_APP_INFO(("T   %2.2f | RH   %2.2f\n", temperature / 1000.0f, humidity / 1000.0f));
@@ -120,12 +127,14 @@ void application_start( )
             WPRINT_APP_INFO(("error reading RH/T values\n"));
         }
 
+        // -- check for data ready flag on SPS30
         ret = sps30_read_data_ready(&data_ready);
         if (ret < 0) {
             WPRINT_APP_INFO(("error %d reading data-ready flag\n", ret));
         }
 
         if (data_ready) {
+            // -- read out SPS30
             ret = sps30_read_measurement(&m);
             if (ret < 0) {
                 WPRINT_APP_INFO(("error reading measurement\n"));
@@ -148,6 +157,7 @@ void application_start( )
        }
 
 #ifdef ENABLE_DATA_UPLOAD
+        // -- send data to cloud dashboard
         if (networkUp == WICED_SUCCESS) {
             if (count % 2 == 0) { // <-- rate limit
                 send_message_http(temperature / 1000.0f, humidity / 1000.0f, tvoc_ppb, co2_eq_ppm,
@@ -158,7 +168,14 @@ void application_start( )
 #endif /* ENABLE_DATA_UPLOAD */
 
 
-        // TODO: add note on timing!
+        // Important: for accurate gas measurements on the SGP30, it is important
+        // to maintain a 1 Hz readout cycle. To ensure this, the following things should be done:
+        //   1. perform the http upload in a separate thread, to avoid going over the 1s
+        //   2. measure the duration of the measurement, and only sleep for the delta of 1s-duration
+        //   3. Potentially move application code unrelated to the SPS30 to another thread
+        //
+        // For the sake of simplicity, the author opted for a simple 1s delay here instead,
+        // potentially sacrificing a little bit of accuracy
         wiced_rtos_delay_milliseconds(1000);
     }
 
